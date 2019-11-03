9fa65907610826fbb19be44a585831c736b7fc23
Dylan Leman <dylanleman@Dylans-MacBook-Pro.local>
2019-11-02 22:46:14 -0600

    login handler 17

--- dao.php
+++ dao.php
@@ -17,8 +17,7 @@ class dao {
 	public function isValidUserName($username){
 		$conn = $this->getConnection();
 		$stmt = $conn->prepare("SELECT * FROM user WHERE name = ?");
-		$stmt->blind_param("s", $usr);
-		$usr = $username;
+		$stmt->blind_param("?", $username);
 		$stmt->execute();
 		//$stmt->store_result();
 		//if($stmt->num_rows>0){
