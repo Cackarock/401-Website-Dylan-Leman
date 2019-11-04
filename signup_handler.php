991444d583bcc4acc0198eb61a5cc8fa8f495304
Dylan Leman <dylanleman@Dylans-MacBook-Pro.local>
2019-11-03 17:48:29 -0700

    sign up 3

--- signup_handler.php
+++ signup_handler.php
@@ -14,9 +14,7 @@ $validUsername = $dba->isValidUsername($username);
 
 $validEmail = preg_match('/\w+@\w+\.[a-zA-Z]{2,4}/', $email);
 
-$validPassword = !(is_null($password));
-
-if(!$validUsername){
+if(!$validUsername || strlen($username) == 0){
 	header("Location: https://arcane-brook-41995.herokuapp.com/sc_login.php");
 	exit;
 }
@@ -26,7 +24,7 @@ if(!$validEmail){
 	exit;
 }
 
-if(!$validPassword){
+if(strlen($password) == 0){
 	header("Location: https://arcane-brook-41995.herokuapp.com/sc_login.php");
 	exit;
 }
