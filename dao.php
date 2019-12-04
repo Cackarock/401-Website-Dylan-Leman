49f3ce1ae818a2b25d1123258cf1b7b54b3d5ab9
Dylan Leman <dylanleman@Dylans-MacBook-Pro.local>
2019-12-04 00:17:26 -0700

    fixing dao

--- dao.php
+++ dao.php
@@ -16,7 +16,7 @@ class dao {
 	
 	public function isValidUser($username, $password){
 		$conn = $this->getConnection();
-		$stmt = $conn->prepare("SELECT * FROM users WHERE userName like :name and password like :password");
+		$stmt = $conn->prepare("SELECT * FROM users WHERE BINARY userName = :name and password = :password");
 		$stmt->bindParam(":name", $username);
 		$stmt->bindParam(":password", $password);
 		$stmt->execute();
